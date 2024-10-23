3dae6cc21708abdfc56e7d679ea261326ff89b5a
Domas Sestakovas <sestakovasdomas@gmail.com>
2024-10-23 04:38:04 +0200

    changed alot

--- src/components/ui/whatsNewCta.tsx
+++ src/components/ui/whatsNewCta.tsx
@@ -1,34 +1,11 @@
-{/* <div className="relative flex space-x-4 items-center z-10 rounded-full bg-zinc-950 py-1 px-4 ring-1 ring-white/10 ">
-                        <span className="text-emerald-300" >
-                          What's new? 
-                        </span>
-                        <span className="">
-                          New training on risk management
-                        </span>
-                        <svg
-                          fill="none"
-                          height="16"
-                          viewBox="0 0 24 24"
-                          width="16"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M10.75 8.75L14.25 12L10.75 15.25"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                          />
-                        </svg>
-                            </div> */}
 
-                            import React from 'react';
+import React from 'react';
 
 const WhatsNewCta: React.FC = () => {
   return (
     <div className="relative flex space-x-4 items-center z-10 rounded-full bg-zinc-950 py-1 px-4 ring-1 ring-white/10 hover:bg-zinc-900 transition ease-in-out duration-300">
       <span className="text-emerald-300">
-        What's new?
+        What&apos;s new?
       </span>
       <span className="text-white">
         New training on risk management
